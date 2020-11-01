cab2710f1a037ee1d56f4934d7507c3dc3b99ee5
rfscholte <rfscholte@apache.org>
2020-11-01 12:33:55 +0100

    Introduce mock repository manager to verify uploaded artifacts

---
+++ src/it/projects/pom-packaging/verify.groovy
@@ -0,0 +1,31 @@
+import java.security.SecureRandom
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+def remoteRepo = new File(basedir, '../../hosted-repo')
+
+def gavFolder = new File(remoteRepo, 'test-group/pom-packaging/1.0.0')
+
+def expectedSignedFiles = ['pom-packaging-1.0.0.pom.asc'] as Set
+					 
+def deployedSignedFiles = [] as Set
+gavFolder.eachFileMatch(groovy.io.FileType.FILES, ~/pom-packaging-1\.0\.0.+\.asc/) { deployedSignedFiles << it.name } 
+
+assert deployedSignedFiles== expectedSignedFiles
